2b4b948d3d006efffb597cb06291e5fbb1dc4253
Dmitry Kostyuchenko <xwolkodavx@mail.ru>
2020-08-13 15:01:30 +0300

    Initial Commit

---
+++ LightBox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LightBox
+//
+//  Created by Dmitry Kostyuchenko on 13.08.2020.
+//  Copyright © 2020 Dmitry Kostyuchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
